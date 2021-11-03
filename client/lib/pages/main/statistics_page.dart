d651fa4a5c658494e1623311778ac156a0a8bf5d
Anthuony <brianpham907@gmail.com>
2021-11-03 14:53:11 -0500

    cleaned look when no sleep sessions exist

--- client/lib/pages/main/statistics_page.dart
+++ client/lib/pages/main/statistics_page.dart
@@ -4,6 +4,7 @@ import 'package:client/widgets/dotted_divider.dart';
 import 'package:client/widgets/fraction_circle.dart';
 import 'package:client/widgets/icon_information.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class StatisticsPage extends StatefulWidget {
   const StatisticsPage({Key? key}) : super(key: key);
@@ -251,20 +252,21 @@ class _StatisticsPageState extends State<StatisticsPage> {
                   ),
                 ),
                 const DottedDivider(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: ListView.builder(
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemExtent: 64,
-                    itemCount: sleepSessions.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return sleepSessions[index];
-                    },
+                if (sleepSessions.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemExtent: 64,
+                      itemCount: sleepSessions.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return sleepSessions[index];
+                      },
+                    ),
                   ),
-                ),
-                const DottedDivider(),
+                if (sleepSessions.isNotEmpty) const DottedDivider(),
                 Container(
                   padding: const EdgeInsets.symmetric(vertical: 16),
                   child: Column(
