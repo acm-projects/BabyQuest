92feae1285e34ca234435d11177041283ae1b2f8
Anthuony <brianpham907@gmail.com>
2021-11-06 13:27:04 -0500

    made todo one page and heavily adjusted appearance

--- client/lib/pages/main/todo_page.dart
+++ client/lib/pages/main/todo_page.dart
@@ -1,4 +1,8 @@
+import 'package:client/models/app_user.dart';
+import 'package:client/models/todo.dart';
+import 'package:client/widgets/dotted_divider.dart';
 import 'package:client/widgets/edit_dialog.dart';
+import 'package:client/widgets/todo_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -18,50 +22,67 @@ class _ToDoPageState extends State<ToDoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('To Do List'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: FaIcon(FontAwesomeIcons.listAlt),
-                text: 'Todos',
+    return Scaffold(
+      body: StreamBuilder(
+        stream: AppUser.updateStream,
+        builder: (context, snapshot) {
+          return SingleChildScrollView(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    colorFilter:
+                        ColorFilter.mode(Color(0x15FFFFFF), BlendMode.dstATop),
+                    image: AssetImage('assets/images/undraw_mother.png'),
+                    fit: BoxFit.cover),
               ),
-              Tab(
-                icon: FaIcon(FontAwesomeIcons.check),
-                text: 'Completed',
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 48),
+                    RichText(
+                      text: TextSpan(
+                          text: 'To Do',
+                          style: Theme.of(context).textTheme.headline1),
+                    ),
+                    const DottedDivider(),
+                    const TodoListWidget(),
+                    const SizedBox(height: 16),
+                    RichText(
+                      text: TextSpan(
+                          text: 'Completed',
+                          style: Theme.of(context).textTheme.headline1),
+                    ),
+                    const DottedDivider(),
+                    const CompletedListWidget(),
+                    const SizedBox(height: 128),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: [
-            TodoListWidget(),
-            CompletedListWidget(),
-          ],
-        ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 24),
-          child: SizedBox(
-            width: 64,
-            height: 64,
-            child: FloatingActionButton(
-              elevation: 0,
-              onPressed: () async {
-                await showEditDialog(
-                    context: context,
-                    label: "Add Todo",
-                    field: const AddTodoDialogWidget(),
-                    updateData: () {},
-                    formKey: _formKey);
-              },
-              backgroundColor: Theme.of(context).colorScheme.secondary,
-              child: Icon(
-                Icons.add,
-                color: Theme.of(context).colorScheme.onSecondary,
-              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 24),
+        child: SizedBox(
+          width: 64,
+          height: 64,
+          child: FloatingActionButton(
+            elevation: 0,
+            onPressed: () async {
+              await showEditDialog(
+                  context: context,
+                  label: "Add Todo",
+                  field: const AddTodoDialogWidget(),
+                  updateData: () {},
+                  formKey: _formKey);
+            },
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+            child: Icon(
+              Icons.add,
+              color: Theme.of(context).colorScheme.onSecondary,
             ),
           ),
         ),
